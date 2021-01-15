97d7ea097a789e9f5e5ef199a9f3357cc601fe69
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-15 10:30:16 -0300

    Concertando bug na activity base

--- base/src/main/java/com/luisfelipe/base/presentation/BaseActivity.kt
+++ base/src/main/java/com/luisfelipe/base/presentation/BaseActivity.kt
@@ -1,23 +1,22 @@
 package com.luisfelipe.base.presentation
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.luisfelipe.base.R
-import com.luisfelipe.movie.R.navigation.movie_navigation
 
 class BaseActivity : AppCompatActivity() {
 
     private lateinit var navigationController: NavController
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
 
-        Toast.makeText(this, "base activity", Toast.LENGTH_SHORT).show()
         setUpNavigation()
         setUpToolbar()
     }
@@ -25,7 +24,7 @@ class BaseActivity : AppCompatActivity() {
     private fun setUpNavigation() {
         supportFragmentManager.findFragmentById(R.id.fragment_container_base_activity)?.let { fragment ->
             navigationController = fragment.findNavController().apply {
-                val graph = navInflater.inflate(movie_navigation)
+                val graph = navInflater.inflate(R.navigation.movie_navigation)
                 setGraph(graph, intent.extras)
             }
         }
