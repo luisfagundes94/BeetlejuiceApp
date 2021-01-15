8e710584dbc8d3357a7b706499ebc4c07dcaf93d
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-15 12:26:48 -0300

    Implementando extensao de imageView com o picasso

--- extensions/src/main/java/com/luisfelipe/extensions/ImageViewExt.kt
+++ extensions/src/main/java/com/luisfelipe/extensions/ImageViewExt.kt
@@ -1,2 +1,8 @@
 package com.luisfelipe.extensions
 
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
+fun ImageView.load(imageUrl: String) {
+    Picasso.get().load(imageUrl).into(this)
+}
