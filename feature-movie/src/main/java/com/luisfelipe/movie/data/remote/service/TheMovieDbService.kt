6a418228dd3326bc74364de5544b9ab1f5acfd62
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-15 12:26:04 -0300

    Adicionando chave de api no serviço

--- feature-movie/src/main/java/com/luisfelipe/movie/data/remote/service/TheMovieDbService.kt
+++ feature-movie/src/main/java/com/luisfelipe/movie/data/remote/service/TheMovieDbService.kt
@@ -1,12 +1,17 @@
 package com.luisfelipe.movie.data.remote.service
 
+import com.luisfelipe.movie.BuildConfig
 import com.luisfelipe.movie.data.remote.model.MovieResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDbService {
 
-    @GET("/movie/{movie_id}")
-    suspend fun getMovieDetails(@Path("movie_id") movieId: Int): Response<MovieResponse>
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") api_key: String = BuildConfig.THE_MOVIE_DB_API_KEY
+    ): Response<MovieResponse>
 }
