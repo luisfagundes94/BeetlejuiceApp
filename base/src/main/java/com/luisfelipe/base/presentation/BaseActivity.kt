bc8cf1c9df4f6843a29edc2c96f7a99605297da9
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-14 22:32:26 -0300

    Configurando BaseActivity para inicializar o Fragment principal

--- base/src/main/java/com/luisfelipe/base/presentation/BaseActivity.kt
+++ base/src/main/java/com/luisfelipe/base/presentation/BaseActivity.kt
@@ -3,11 +3,33 @@ package com.luisfelipe.base.presentation
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.luisfelipe.base.R
+import com.luisfelipe.movie.R.navigation.movie_navigation
 
 class BaseActivity: AppCompatActivity() {
+
+    private lateinit var navigationController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_base)
+
+        setUpNavigation()
+        setUpToolbar()
+    }
+
+    private fun setUpNavigation() {
+        supportFragmentManager.findFragmentById(R.id.fragment_container_base_activity)?.let { fragment ->
+            navigationController = fragment.findNavController().apply {
+                val graph = navInflater.inflate(movie_navigation)
+                setGraph(graph, intent.extras)
+            }
+        }
+    }
+
+    private fun setUpToolbar() {
+        supportActionBar?.hide()
     }
 }
--- feature-movie/src/main/java/com/luisfelipe/movie/presentation/details/DetailsFragment.kt
+++ feature-movie/src/main/java/com/luisfelipe/movie/presentation/details/DetailsFragment.kt
@@ -1,8 +1,14 @@
 package com.luisfelipe.movie.presentation.details
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.luisfelipe.movie.R
 
 class DetailsFragment: Fragment(R.layout.fragment_details) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("details", "hello!")
+    }
 }
