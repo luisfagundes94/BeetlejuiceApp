7d5f11511abe7c4c296fa29ba3d6f000e1e61bd2
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-15 12:25:07 -0300

    Adicionando url base no backdrop de filme

--- feature-movie/src/main/java/com/luisfelipe/movie/data/mapper/MovieMapper.kt
+++ feature-movie/src/main/java/com/luisfelipe/movie/data/mapper/MovieMapper.kt
@@ -1,15 +1,17 @@
-package com.luisfelipe.movie.data.remote.mapper
+package com.luisfelipe.movie.data.mapper
 
 import com.luisfelipe.movie.data.remote.model.MovieResponse
 import com.luisfelipe.movie.domain.model.Movie
 
 class MovieMapper {
     companion object {
+        const val BASE_BACKDROP_PATH = "https://image.tmdb.org/t/p/w500"
+
         fun mapResponseToDomain(movieResponse: MovieResponse): Movie {
             return Movie(
                 id = movieResponse.id,
                 title = movieResponse.title,
-                backdrop = movieResponse.backdrop_path,
+                backdrop = BASE_BACKDROP_PATH + movieResponse.backdrop_path,
                 views = movieResponse.popularity,
                 likes = movieResponse.vote_count
             )
