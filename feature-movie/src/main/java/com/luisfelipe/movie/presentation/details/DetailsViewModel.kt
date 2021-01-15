eeb5cf3b7de0ce7992ffadfee3e4d681dddeccc2
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-15 12:23:55 -0300

    Implementando viewModel

--- feature-movie/src/main/java/com/luisfelipe/movie/presentation/details/DetailsViewModel.kt
+++ feature-movie/src/main/java/com/luisfelipe/movie/presentation/details/DetailsViewModel.kt
@@ -1,7 +1,27 @@
 package com.luisfelipe.movie.presentation.details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.luisfelipe.movie.domain.enums.ResultStatus
+import com.luisfelipe.movie.domain.model.Movie
+import com.luisfelipe.movie.domain.usecase.GetMovieDetailsFromApi
+import kotlinx.coroutines.launch
 
-class DetailsViewModel: ViewModel() {
+class DetailsViewModel(
+    private val getMovieDetailsFromApi: GetMovieDetailsFromApi
+): ViewModel() {
 
+    private val movieDetailsResultStatusLiveData = MutableLiveData<ResultStatus<Movie>>()
+    val movieDetailsResultStatus: LiveData<ResultStatus<Movie>> = movieDetailsResultStatusLiveData
+
+    private companion object {
+        const val BEETLEJUICE_ID = 4011
+    }
+
+    fun getMovieDetails() = viewModelScope.launch {
+        val movieDetailsResultStatus = getMovieDetailsFromApi(BEETLEJUICE_ID)
+        movieDetailsResultStatusLiveData.postValue(movieDetailsResultStatus)
+    }
 }
