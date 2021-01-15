bdb9ed5c3be321d1f106377b1abd4cec849315de
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-15 10:41:01 -0300

    Configurando viewBinding no fragment

--- feature-movie/src/main/java/com/luisfelipe/movie/presentation/details/DetailsFragment.kt
+++ feature-movie/src/main/java/com/luisfelipe/movie/presentation/details/DetailsFragment.kt
@@ -2,13 +2,33 @@ package com.luisfelipe.movie.presentation.details
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.luisfelipe.movie.R
+import com.luisfelipe.movie.databinding.FragmentDetailsBinding
 
 class DetailsFragment: Fragment(R.layout.fragment_details) {
 
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
     }
 }
