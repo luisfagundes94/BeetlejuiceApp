c2a01db758888c9a9b55dfa54b6f7bfdc3057d54
Luís Felipe Fagundes <luisfelipecf94@gmail.com>
2021-01-17 13:44:06 -0300

    Refatorando modelo de resposta

--- feature-movie/src/main/java/com/luisfelipe/movie/data/remote/model/SimilarMovieResponse.kt
+++ feature-movie/src/main/java/com/luisfelipe/movie/data/remote/model/SimilarMovieResponse.kt
@@ -1,8 +1,10 @@
 package com.luisfelipe.movie.data.remote.model
 
+import com.google.gson.annotations.SerializedName
+
 data class SimilarMovieResponse(
     val title: String,
-    val release_date: String,
-    val poster_path: String,
-    val genre_ids: List<Int>
+    @SerializedName("release_date") val releaseDate: String,
+    @SerializedName("posterPath") val posterPath: String,
+    @SerializedName("genre_ids") val genreIds: List<Int>
 )
